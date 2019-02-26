382810054e38c24f1f76056523fb8bae8d7493c1
Nikos Kiokpasoglou <Nikolaos.Kiokpasoglou@stormid.com>
2019-02-26 10:55:20 +0000

    normalize routes

--- src/Client/IHealthDataClient.cs
+++ src/Client/IHealthDataClient.cs
@@ -9,7 +9,7 @@ namespace MyBp.Client
     [Headers("Authorization: Bearer")]
     public interface IHealthDataClient
     {
-        [Get("/query/v1")]
+        [Get("/sample/v1")]
         Task<HealthSamplesDto> ExecuteQueryAsync(
             [Query(CollectionFormat.Multi)] IEnumerable<string> types,
             [Query(Format = "O")] Dictionary<string, object> queryDateParams,
